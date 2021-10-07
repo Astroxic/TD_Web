843b86d90909fb9b72e6b561cc0891f6a396b8d3
Vincent Pilard <vpilard@sio.local>
2021-10-07 17:06:49 +0200

    Modif+Web-arena

---
+++ Slam/HTML/Formulaire/submitForm.php
@@ -0,0 +1,14 @@
+<?php
+    if(count($_POST)>0){
+?>
+    <h1>Contenu du POST</h1>
+    <ul>   
+        <li><?=$_POST["nom"]?></li>
+        <li><?=$_POST["email"]?></li> 
+        <li><?=$_POST["pwd"]?></li> 
+    </ul>
+    <?php
+}else{
+    echo "Accès direct interdit"
+}
+?>
