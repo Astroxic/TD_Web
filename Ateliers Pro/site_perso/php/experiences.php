4afb3d5dc7a59ae73cd8fd39766850ff687a73bb
Vin's <vincent.pilard@sts-sio-caen.info>
2021-12-04 22:50:30 +0100

    Test

--- Ateliers Pro/site_perso/php/experiences.php
+++ Ateliers Pro/site_perso/php/experiences.php
@@ -1,12 +1,10 @@
 <?php
 $experiences=\file_get_contents('./yaml/experiences.yaml');
-$data=yaml_parse_file($experiences);
+$data=yaml_parse($experiences);
 ?>
 <h1>Expériences</h1>
 <p>
-    <?php echo $data["poste"]."<br/>";?>
-    <?php echo $data["entreprise"]." à ".$data["lieu"]."<br/>";?>
-    <?php echo "De ".$data["dateDebut"]." à ".$data["dateFin"]."<br/>";?>
-    <?php echo $data["descriptif"]."<br/>";?>
-    <?php echo "</div>";?>
+    En <?php echo $data['dateDebut']?> jusqu'en <?php echo $data['dateFin']?>, j'ai efféctué un stage en entreprise.<br>
+    Celle-ci s'appelait <?php echo $data["entreprise"]?> et se trouvait à <?php echo $data["lieu"]?><br/>
+    <?php echo $data['descriptif']?>.
 </p>
